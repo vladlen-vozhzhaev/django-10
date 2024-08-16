ff7e2a8f277d8f0576b16955ac7912d5f7474fa0
Vladlen <vladlen@vozhzhaev.ru>
2024-08-16 15:30:06 +0300

    first commit

---
+++ myproject/myapp/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponse
+import random
+
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(req):
+    return render(req, "index.html")
+
+def game(request):
+    num1 = random.randint(0,3)
+    num2 = random.randint(0, 3)
+    num3 = random.randint(0, 3)
+    if num1 == num2 == num3:
+        response = f"Победа, все 3 числа равны! Числа: {num1}, {num2}, {num3}"
+    else:
+        response = f"Повезёт в следующий раз! Числа: {num1}, {num2}, {num3}"
+    return HttpResponse(response)
+
+def get_phrase1(req):
+    return HttpResponse("Первая фраза")
+
+def get_phrase2(req):
+    return HttpResponse("Вторая фраза")
